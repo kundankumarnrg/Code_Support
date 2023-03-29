be0a7f3c37cbcfe68b7304732f4d3457caae08d5
kundan kumar <119260880+kundankumarnrg@users.noreply.github.com>
2023-03-29 21:27:34 +0530

    Update and rename Error & Solution.txt to Error & Solution.java

---
+++ Java/Error & Solution.java
@@ -0,0 +1,31 @@
+1) I had the same issue. I was using eclipse platform and server was missing in my show view.
+   Steps:
+    -help->install new software
+    -in work with : select : "Indigo Update Site - http://download.eclipse.org/releases/indigo/" , once selected, all available software will be displayed in the   
+     section under type filter text
+    -Expand “Web, XML, and Java EE Development” and select "JST Server adapters extensions"
+
+
+1) I had the same issue. I was using eclipse platform and server was missing in my show view.
+   Steps:
+   
+1) I had the same issue. I was using eclipse platform and server was missing in my show view.
+   Steps:
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
