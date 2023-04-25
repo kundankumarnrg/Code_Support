dbf374305c5a11f10b50ae5ae16c65658d5df0ef
kundan kumar <119260880+kundankumarnrg@users.noreply.github.com>
2023-04-25 10:18:04 +0530

    Update Error & Solution.java

--- Java/Error & Solution.java
+++ Java/Error & Solution.java
@@ -1,36 +1,33 @@
-1) I had the same issue. I was using eclipse platform and server was missing in my show view.
+>> I had the same issue. I was using eclipse platform and server was missing in my show view.
    Steps:
-    -help->install new software
-    -in work with : select : "Indigo Update Site - http://download.eclipse.org/releases/indigo/" , once selected, all available software will be displayed in the   
-     section under type filter text
-    -Expand “Web, XML, and Java EE Development” and select "JST Server adapters extensions"
-
-
-2) How to see my Eclipse version?
-   Steps:
-      -Help-->installation details
-      
-   
-3) Server Option is not Showing in Eclipse:
-   Steps:
-     -Open Eclipse and move move 
-       [window -> show view -> ohters -> show views menu select ->other[If here "server" is not showing]
-     -Check current version of Eclipse
-     -Open the below like given and select your version and copy urls link.
-     -move some other menu:
-       [help -> install new software -> Avilable software ->work with -><past copy link>
-     -select web,xml,java,EE and OSGI enterprises Deelopment->next and finished
+   -help->install new software
+   -in work with : select : "Indigo Update Site - http://download.eclipse.org/releases/indigo/" , once selected, all available software will be displayed in the   
+    section under type filter text
+   -Expand “Web, XML, and Java EE Development” and select "JST Server adapters extensions"
        
+    OR
+    
+   -Open Eclipse and move move 
+    [window -> show view -> ohters -> show views menu select ->other[If here "server" is not showing]
+   -Check current version of Eclipse
+   -Open the below like given and select your version and copy urls link.
+   -move some other menu:
+    [help -> install new software -> Avilable software ->work with -><past copy link>
+   -select web,xml,java,EE and OSGI enterprises Deelopment->next and finished
+
+
+>>How to see my Eclipse version?
+   Steps:
+   -Help-->installation details      
    
 
-4) Tomcat configure in Eclipse:
+>> Tomcat configure in Eclipse:
    Steps:
        -Open Eclipse
         [window -> show view -> other view -> server ->open 
         
 
-
-5) Set Servlet-api.jar file path:
+>> Set Servlet-api.jar file path:
    Steps:
       -open Eclipse:
        right click project explore->properties ->java build path ->select class path -> Add External jarfile
@@ -38,25 +35,27 @@
       After moving 
       C:\Program Files\Apache Software Foundation\Tomcat 10.0\lib -> Select ->servlet-api.jar file ->Apply and close
          
-
-
-6) 
+>> 
+   Steps:
+         
+         
+>> 
    Steps:
 
 
-7) 
+>> 
    Steps:
 
 
-8) 
+>> 
    Steps:
 
 
-9) 
+>> 
    Steps:
 
 
-10) 
+>> 
    Steps:
    
 
